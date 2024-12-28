5a14ff8b48a8a37ab78b9392a6c53474860cef90
mehdreaming <mehdreaming@gmail.com>
2024-12-28 18:03:41 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 class Config {
   constructor() {
-    this.baseURL = 'https://nodepay.org';
+    this.baseURL = 'https://app.nodepay.ai';
     this.ipCheckURL = 'https://ipinfo.io/json';
     this.pingURL = 'http://nw.nodepay.ai/api/network/ping';
     this.retryInterval = 30000; // 30 seconds

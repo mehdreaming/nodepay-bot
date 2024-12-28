b88b1b88854fc00999e88aa6ccf4b544de2023ad
mehdreaming <mehdreaming@gmail.com>
2024-12-28 14:07:30 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -17,8 +17,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=          Nodepay Airdrop Bot         ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=     Created by mehdreaming           ='.cyan);
+  console.log('=    https://t.me/mehdreaming          ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }

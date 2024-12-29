dbc10271a302a6be3833156539e27ad8fd2ccc09
mehdreaming <mehdreaming@gmail.com>
2024-12-29 19:23:24 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,36 +1,11 @@
-// config.js
+class Config {
+  constructor() {
+    this.baseURL = 'https://nodepay.org';
+    this.ipCheckURL = 'https://ipinfo.io/json';
+    this.pingURL = 'http://nw.nodepay.ai/api/network/ping';
+    this.retryInterval = 30000;
+    this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
+  }
+}
 
-const CONFIG = {
-  // API Endpoints
-  API: {
-    SESSION: "https://api.nodepay.org/api/auth/session",
-    PING: "https://nw.nodepay.org/api/network/ping",
-  },
-
-  // Connection States
-  CONNECTION_STATES: {
-    CONNECTED: 1,
-    DISCONNECTED: 2, // Server error
-    NONE_CONNECTION: 3, // Case logout
-  },
-
-  // Timeout settings
-  TIMEOUTS: {
-    DEFAULT_PING_TIMEOUT: 55 * 60 * 1000, // 55 minutes
-    RETRY_PING_TIMEOUT: 5 * 60 * 1000, // 5 minutes
-  },
-
-  // Random time settings
-  RANDOM_TIME: {
-    MIN_MINUTES: 1,
-    MAX_MINUTES: 5,
-  },
-
-  // Debugging settings
-  DEBUG: false, // Set to true for debugging logs
-
-  // Other configurations
-  MAX_RETRIES: 6,
-};
-
-module.exports = CONFIG;
+module.exports = Config;
